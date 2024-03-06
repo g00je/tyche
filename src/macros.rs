c9e314f53835b828df01d4c0abbe99403cf921ce
007 <dr007cc@gmail.com>
2024-03-07 01:25:05 +0330

    feat: add a X macro for listing all the models

---
+++ src/macros.rs
@@ -0,0 +1,23 @@
+
+#[macro_export]
+macro_rules! act_on_list {
+    ([$($m:ident),*], $g:ident) => { $( $g! ( $m ); )* }
+}
+
+#[macro_export]
+macro_rules! act_on_models {
+    ($($mac:tt)*) => {
+        macro_rules! act { $($mac)* }
+
+        crate::macros::act_on_list! {
+            // [ResponseHead, Gene, Detail, Record, Agent, Duration,
+            //  Eatery, Dish, Review, ReviewData, BlockHeader, ReviewBlock,
+            //  MenuBlock, SessionInfo, Session, User, UserLoginArgs],
+            [Gene, Duration, Eatery, SessionInfo, Session],
+            act
+        }
+    }
+}
+
+pub(crate) use act_on_list;
+pub(crate) use act_on_models;
